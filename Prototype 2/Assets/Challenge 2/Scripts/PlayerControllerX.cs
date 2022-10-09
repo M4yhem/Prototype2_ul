feb5e73ece694348f008e2b98c7780772c02ed2c
1rg0 <igor.fernandes.matos@hotmail.com>
2022-10-08 22:56:58 -0300

    Prototype 2 from Unity Learn

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float time = 2f;
+    float timer;
+    private void Start()
+    {
+        timer = Time.time;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= time)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // On spacebar press, send dog
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timer = 0f;
+            }
+        }
+    }
+}
